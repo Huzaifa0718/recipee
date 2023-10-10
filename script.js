aad1ece61be523367c7d33d61f0da4e426df5c47
Huzaifa0718 <146505580+Huzaifa0718@users.noreply.github.com>
2023-10-10 06:17:28 +0000

    styled it

--- script.js
+++ script.js
@@ -1,45 +1,72 @@
-let box = document.getElementById("dish")
-let x = document.getElementById("text")
+//Initial References
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-let recipee = () => {
-let Dish = box.value;
-if(Dish.length == 0){
-  x.innerHTML = `<h1>enter the dish</h1>`
-}
-else{
-let p = fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=" + Dish +"&" )
-p.then( response => response.json())
-.then((data) => {
-  console.log(data)
-  
-  for(i=0;i< data.meals.length;i++){
-      console.log(data.meals[i].strMeal)
-      console.log(data.meals[i].strArea)
-  console.log(data.meals[i].strIngredient1,data.meals[i].strMeasure1)
-    console.log(data.meals[i].strIngredient2,data.meals[i].strMeasure2)
-    console.log(data.meals[i].strIngredient3,data.meals[i].strMeasure3)
-    console.log(data.meals[i].strIngredient4,data.meals[i].strMeasure4)
-    console.log(data.meals[i].strIngredient5,data.meals[i].strMeasure5)
-    console.log(data.meals[i].strIngredient6,data.meals[i].strMeasure6)
-console.log(data.meals[i].strIngredient7,data.meals[i].strMeasure7)
-    console.log(data.meals[i].strIngredient8,data.meals[i].strMeasure8)
-    console.log(data.meals[i].strIngredient9,data.meals[i].strMeasure9)
-  console.log(data.meals[i].strIngredient10,data.meals[i].strMeasure10)
-    console.log(data.meals[i].strIngredient11,data.meals[i].strMeasure11)
-    console.log(data.meals[i].strIngredient12,data.meals[i].strMeasure12)
-    console.log(data.meals[i].strIngredient13,data.meals[i].strMeasure13)
-    console.log(data.meals[i].strIngredient14,data.meals[i].strMeasure14)
-    console.log(data.meals[i].strIngredient15,data.meals[i].strMeasure15)
-    console.log(data.meals[i].strIngredient16,data.meals[i].strMeasure16)
-    console.log(data.meals[i].strIngredient17,data.meals[i].strMeasure17)
-               console.log(data.meals[i].strIngredient18,data.meals[i].strMeasure18)
-      console.log(data.meals[i].strIngredient19,data.meals[i].strMeasure19)
-    console.log(data.meals[i].strInstructions)
+searchBtn.addEventListener("click", () => {
+  let userInp = document.getElementById("user-inp").value;
+  if (userInp.length == 0) {
+    result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
+  } else {
+    fetch(url + userInp)
+      .then((response) => response.json())
+      .then((data) => {
+        let myMeal = data.meals[0];
+        console.log(myMeal);
+        console.log(myMeal.strMealThumb);
+        console.log(myMeal.strMeal);
+        console.log(myMeal.strArea);
+        console.log(myMeal.strInstructions);
+        let count = 1;
+        let ingredients = [];
+        for (let i in myMeal) {
+          let ingredient = "";
+          let measure = "";
+          if (i.startsWith("strIngredient") && myMeal[i]) {
+            ingredient = myMeal[i];
+            measure = myMeal[`strMeasure` + count];
+            count += 1;
+            ingredients.push(`${measure} ${ingredient}`);
+          }
+        }
+        console.log(ingredients);
 
-    
+        result.innerHTML = `
+    <img src=${myMeal.strMealThumb}>
+    <div class="details">
+        <h2>${myMeal.strMeal}</h2>
+        <h4>${myMeal.strArea}</h4>
+    </div>
+    <div id="ingredient-con"></div>
+    <div id="recipe">
+        <button id="hide-recipe">X</button>
+        <pre id="instructions">${myMeal.strInstructions}</pre>
+    </div>
+    <button id="show-recipe">View Recipe</button>
+    `;
+        let ingredientCon = document.getElementById("ingredient-con");
+        let parent = document.createElement("ul");
+        let recipe = document.getElementById("recipe");
+        let hideRecipe = document.getElementById("hide-recipe");
+        let showRecipe = document.getElementById("show-recipe");
+
+        ingredients.forEach((i) => {
+          let child = document.createElement("li");
+          child.innerText = i;
+          parent.appendChild(child);
+          ingredientCon.appendChild(parent);
+        });
+
+        hideRecipe.addEventListener("click", () => {
+          recipe.style.display = "none";
+        });
+        showRecipe.addEventListener("click", () => {
+          recipe.style.display = "block";
+        });
+      })
+      .catch(() => {
+        result.innerHTML = `<h3>Invalid Input</h3>`;
+      });
   }
-})
-}
-}
-
+});
 
